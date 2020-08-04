36171d6854e95f58eb2f22e11c47d40ccfbf8c3c
olivier.lediouris <olivier.lediouris@oracle.com>
2020-08-04 09:14:18 -0700

    SVG wind roses.

--- web.stuff/boat.stuff/trip-2010-2011/js/script.js
+++ web.stuff/boat.stuff/trip-2010-2011/js/script.js
@@ -248,3 +248,159 @@ function reset(nodeName) {
     alert("Exception! " + ex.toString()); 
   } 
 }
+
+// LeafLet utilities
+if (Math.toRadians === undefined) {
+  Math.toRadians = deg => {
+    return (deg / 180) * Math.PI;
+  };
+}
+
+// Special characters, see https://www.toptal.com/designers/htmlarrows/numbers/fraction-one-quarter/
+const NORTH = { value: 0, label: "N" };
+const NORTH_1_4_NORTH_EAST = { value: 11.25, label: "N\u00BCNE" };
+const NORTH_NORTH_EAST = { value: 22.5, label: "NNE" };
+const NORTH_EAST_1_4_NORTH = { value: 33.75, label: "N\u00BCNE" };
+const NORTH_EAST = { value: 45, label: "NE" };
+const NORTH_EAST_1_4_EAST = { value: 56.25, label: "N\u00BCNE" };
+const EAST_NORTH_EAST = { value: 67.5, label: "ENE" };
+const EAST_1_4_NORTH_EAST = { value: 68.75, label: "E\u00BCNE" };
+const EAST = { value: 90, label: "E" };
+const EAST_1_4_SOUTH_EAST = { value: 101.25, label: "E\u00BCSE" };
+const EAST_SOUTH_EAST = { value: 112.50, label: "ESE" };
+const SOUTH_EAST_1_4_EAST = { value: 123.75, label: "SE\u00BCE" };
+const SOUTH_EAST = { value: 135, label: "SE" };
+const SOUTH_EAST_1_4_SOUTH = { value: 146.25, label: "SE\u00BCS" };
+const SOUTH_SOUTH_EAST = { value: 157.50, label: "SSE" };
+const SOUTH_1_4_SOUTH_EAST = { value: 168.75, label: "S\u00BCSE" };
+const SOUTH = {value: 180, label: "S"};
+const SOUTH_1_4_SOUTH_WEST = {value: 191.25, label: "S\u00BCSW"};
+const SOUTH_SOUTH_WEST = {value: 202.50, label: "SSW"};
+const SOUTH_WEST_1_4_SOUTH = {value: 213.75, label: "SW\u00BCS"};
+const SOUTH_WEST = {value: 225, label: "SW"};
+const SOUTH_WEST_1_4_WEST = {value: 236.25, label: "SW\u00BCW"};
+const WEST_SOUTH_WEST = {value: 247.50, label: "WSW"};
+const WEST_1_4_SOUTH_WEST = {value: 258.75, label: "W\u00BCSW"};
+const WEST = {value: 270, label: "W"};
+const WEST_1_4_NORTH_WEST = {value: 281.25, label: "W\u00BCNW"};
+const WEST_NORTH_WEST = {value: 292.50, label: "WNW"};
+const NORTH_WEST_1_4_WEST = {value: 303.75, label: "NW\u00BCW"};
+const NORTH_WEST = {value: 315, label: "NW"};
+const NORTH_WEST_1_4_NORTH = {value: 326.25, label: "NW\u00BCN"};
+const NORTH_NORTH_WEST = {value: 337.50, label: "NNW"};
+const NORTH_1_4_NORTH_WEST = {value: 348.75, label: "SN\u00BCNW"};
+
+const WIND_COLORS = [
+  'rgb(255, 255, 255)', // 0-5
+  'rgb(21, 200, 232)',  // Blue 5-10
+  'rgb(19, 234, 186)',  // Lighter blue 10-15
+  'rgb(48, 232, 21)',   // Green 15-20
+  'rgb(211, 239, 14)',  // Yellow 20-25
+  'rgb(232, 180, 21)',  // Orange 25-30
+  'rgb(232, 100, 21)',  // Darker Orange 30-35
+  'rgb(180, 8, 0)',     // Red 35-40
+  'rgb(147, 4, 0)',     // Dark red 40-45
+  'rgb(148, 4, 161)'    // Purple 45-
+];
+
+function getWindColor(beaufort) {
+  let colorIdx = Math.min(parseInt(beaufort.toFixed(0)), WIND_COLORS.length - 1);
+  return WIND_COLORS[colorIdx];
+}
+
+const FRAME_WIDTH  = 160;
+const FRAME_HEIGHT = 160;
+
+function drawWindArrow(divId, dir, force) {
+  let label = '';
+  let fillColor = 'lime';
+  if (force !== undefined) {
+    label = `F ${force}, `;
+    fillColor = getWindColor(force);
+  }
+  let direction;  // = dir; //  + 180;
+  if (typeof(dir) === 'number') {
+    direction = dir;
+    label += `${dir}°`
+  } else {
+    try {
+      direction = dir.value;
+      label += dir.label;
+    } catch (oops) {
+      console.log('Akeu what?')
+      direction = 0;
+    }
+  }
+
+  direction = -direction;
+  // XMLNS Required.
+  const XMLNS = "http://www.w3.org/2000/svg";
+
+  let parent = (divId !== null) ? document.getElementById(divId) : null;
+  let svg = document.createElementNS(XMLNS, 'svg');
+  // svg.setAttribute('xmlns', xmlns);
+  svg.setAttributeNS(null, 'width', FRAME_WIDTH.toString());
+  svg.setAttributeNS(null, 'height', FRAME_HEIGHT.toString());
+  svg.setAttribute('style', 'background-color: rgba(255, 255, 255, 0); border-radius: 10px; border: 1px solid blue;');
+
+  if (parent !== null) {
+    parent.appendChild(svg);
+  }
+
+  let circle = document.createElementNS(XMLNS, 'circle');
+  circle.setAttributeNS(null, 'cx', (FRAME_WIDTH / 2).toString());
+  circle.setAttributeNS(null, 'cy', (FRAME_HEIGHT / 2).toString());
+  circle.setAttributeNS(null, 'r', '40');
+  circle.setAttributeNS(null, 'stroke', 'rgba(0, 255, 0, 1)');
+  circle.setAttributeNS(null, 'stroke-width', '4');
+  circle.setAttributeNS(null, 'fill', 'rgba(0, 255, 255, 0.25)');
+  svg.appendChild(circle);
+
+  let polygon = document.createElementNS(XMLNS, 'polygon');
+  polygon.setAttribute('style', `fill: ${fillColor}; stroke: blue; stroke-width: 2;`);
+
+  let headX = (FRAME_WIDTH / 2) + (60 * Math.sin(Math.toRadians(direction)));
+  let headY = (FRAME_HEIGHT / 2) + (60 * Math.cos(Math.toRadians(direction)));
+  let arrow = [{
+    // head
+    x: headX,
+    y: headY
+  }, {
+    // tail - left
+    x: (FRAME_WIDTH / 2) - (60 * Math.sin(Math.toRadians(direction + 10))),
+    y: (FRAME_HEIGHT / 2) - (60 * Math.cos(Math.toRadians(direction + 10)))
+  }, {
+    // tail - center
+    x: (FRAME_WIDTH / 2) - (55 * Math.sin(Math.toRadians(direction))),
+    y: (FRAME_HEIGHT / 2) - (55 * Math.cos(Math.toRadians(direction)))
+  }, {
+    // tail - right
+    x: (FRAME_WIDTH / 2) - (60 * Math.sin(Math.toRadians(direction - 10))),
+    y: (FRAME_HEIGHT / 2) - (60 * Math.cos(Math.toRadians(direction - 10)))
+  }];
+  // Draw polygon points here
+  let points = ""; // `${head.x.toFixed(0)},${head.y.toFixed(0)} ${tailRight.x.toFixed(0)},${tailRight.y.toFixed(0)} ${tail.x.toFixed(0)},${tail.y.toFixed(0)} ${tailLeft.x.toFixed(0)},${tailLeft.y.toFixed(0)}`;
+  arrow.forEach(pt => {
+    points += `${pt.x.toFixed(0)},${pt.y.toFixed(0)} `;
+  });
+  // console.log('Points:' + points.trim());
+  polygon.setAttributeNS(null, 'points', points.trim());
+  svg.appendChild(polygon);
+
+  let text = document.createElementNS(XMLNS, 'text');
+  text.setAttributeNS(null, 'x', '10');
+  text.setAttributeNS(null, 'y', '25');
+  text.setAttributeNS(null, 'font-size', '16');
+  text.setAttributeNS(null, 'font-family', "'Helvetica Neue', 'Lato', Verdana, Helvetica, Geneva, sans-serif;");
+  text.setAttributeNS(null, 'fill', 'blue');
+  text.appendChild(document.createTextNode(`${label}`));
+  svg.appendChild(text);
+
+  if (parent !== null) {
+    console.log(parent.innerHTML);
+  }
+  //debugger;
+  return { x: headX, y: headY, svgContent: svg };
+}
+
+
